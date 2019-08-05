258af8a3c388903a7318ff0fde4ba00a9aa756c3
Shivani Singhal <shivani.singhal@Shivanisinghal1.local>
2019-08-05 23:19:58 +0530

    How to sync and resolve conflicts of your branch with master

---
+++ git/script.sh
@@ -0,0 +1,13 @@
+## to sync your current branch with master 
+# current branch is :: my_branch
+
+git checkout master 
+git pull origin master 
+
+# then move to your branch 
+git checkout my_branch 
+git merge --no-commi --no-ff master 
+# then resolve conflicts
+
+
+ 
