786a60efbeb7d16d8aa59c2c6185bcbae55e10c5
shivi28 <shivani.singhal2804@gmail.com>
2020-03-29 15:01:09 +0530

    Update script.sh

--- git/script.sh
+++ git/script.sh
@@ -9,5 +9,12 @@ git checkout my_branch
 git merge --no-commit --no-ff master 
 # then resolve conflicts
 
-
+**How to rebase your current branch from root branch
+```
+# on master branch 
+git checkout -b feature_branch 
+# move to feature_branch and then do dvelopment by pushing multiple commits
+git rebase -i origin/master~2
+git push origin feature_branch_name --force
+```
  
